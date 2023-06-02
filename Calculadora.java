2c034f4123b9104d648faa120e26b28f7598ca9e
Shirleybarbosa07 <135287037+Shirleybarbosa07@users.noreply.github.com>
2023-06-01 23:15:06 -0300

    implementando classe de divisão e operação

--- Calculadora.java
+++ Calculadora.java
@@ -27,6 +27,28 @@ class Multiplicacao {
     public static double executar(double a, double b) {
         return a * b;
     }
+    public class Calculadora {
+    public double somar(double num1, double num2) {
+        return num1 + num2;
+    }
+    
+    public double subtrair(double num1, double num2) {
+        return num1 - num2;
+    }
+    
+    public double multiplicar(double num1, double num2) {
+        return num1 * num2;
+    }
+    
+    public double dividir(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            throw new IllegalArgumentException("Erro: Divisão por zero não é permitida.");
+        }
+    }
+}
+
 }
 
 // Classe principal que utiliza as operações da calculadora
@@ -55,5 +77,28 @@ public class Calculadora {
 
         double resultadoMultiplicacaoDouble = Multiplicacao.executar(3.5, 2.1);
         System.out.println("Resultado da multiplicação (double): " + resultadoMultiplicacaoDouble);
+
+        public class Calculadora {
+    public double somar(double num1, double num2) {
+        return num1 + num2;
+    }
+    
+    public double subtrair(double num1, double num2) {
+        return num1 - num2;
+    }
+    
+    public double multiplicar(double num1, double num2) {
+        return num1 * num2;
+    }
+    
+    public double dividir(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            throw new IllegalArgumentException("Erro: Divisão por zero não é permitida.");
+        }
+    }
+}
+
     }
 }
